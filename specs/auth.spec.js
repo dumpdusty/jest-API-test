77f80ec3242f4e26c6d12a46012a2975dcc78acd
Dusty Dump <dumpdusty@gmail.com>
2023-06-28 15:25:29 -0700

    initial commit

---
+++ specs/auth.spec.js
@@ -0,0 +1,21 @@
+import {request} from "../common/request";
+
+describe('Authorization & Authentication', () => {
+    test('Sign in with valid credentials', async () => {
+        const response = await request.post('/auth', {login: 'adminius', password: 'supers3cret'})
+        expect(response.status).toEqual(200)
+        expect(response.data).toEqual({token: expect.any(String)})
+    });
+
+    it('Sign in with invalid login', async () => {
+        const response = await request.post('/auth', {login: 'adminius1', password: 'supers3cret'})
+        expect(response.status).toEqual(404)
+        expect(response.data).toEqual({message: 'Wrong login or password.'})
+    });
+
+    it('Sign in without data', async () => {
+        const response = await request.post('/auth', {})
+        expect(response.status).toEqual(400)
+        expect(response.data).toEqual({message: 'No credentials provided.'})
+    });
+});
